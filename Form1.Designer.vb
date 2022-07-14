e7ac67b615135bda27cb1c5733983952335f8bcc
Walker Dick <walkerraydick2@gmail.com>
2022-07-14 14:34:09 -0500

    COM port work

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,13 +23,45 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CB_Sports = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
+        '
+        'CB_Sports
+        '
+        Me.CB_Sports.FormattingEnabled = True
+        Me.CB_Sports.Location = New System.Drawing.Point(13, 13)
+        Me.CB_Sports.Name = "CB_Sports"
+        Me.CB_Sports.Size = New System.Drawing.Size(227, 21)
+        Me.CB_Sports.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(374, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Open COM Port"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1200", "2400", "4800", "9600", "14400", "19200", "28800", "38400", "57600", "115200", "230400"})
+        Me.ComboBox1.Location = New System.Drawing.Point(247, 13)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(633, 499)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CB_Sports)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Scanner Watch"
@@ -37,4 +69,7 @@ Partial Class Form1
 
     End Sub
 
+    Friend WithEvents CB_Sports As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.Threading
+Public Class Form1
 
+
+    Sub GetSerialPortNames()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            CB_Sports.Items.Add(sp)
+        Next
+    End Sub
+    Sub ReadSerialPort(Port As String, Baudrate As Integer)
+        My.Computer.Ports.OpenSerialPort(Port, Baudrate)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        GetSerialPortNames()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
